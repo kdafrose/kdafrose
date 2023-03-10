59c67af624c86018b8b32b321fc11deef482703a
kdafrose <katherine.tubang@ucalgary.ca>
2023-03-09 18:55:48 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React from "react";
 import Navbar from "./Navbar";
 import Main from "./components/Main";
 import EditNotes from  "./components/EditNotes";
+import View from "./components/View";
+
 
 
 function App() {
 
   return (
+    
     <>
       <BrowserRouter>
         <Navbar />
+        
         <Routes>
           <Route exact path ="/" element = {<Main />}/> 
-          <Route exact path ="/notes/:id/edit" element = {<EditNotes />}/>
+          <Route exact path ="/notes/:id/edit" element = {<EditNotes   />}/>
+          <Route exact path = "/notes/:id" element = {<View   />}/>
         </Routes>
       </BrowserRouter>
     </>
